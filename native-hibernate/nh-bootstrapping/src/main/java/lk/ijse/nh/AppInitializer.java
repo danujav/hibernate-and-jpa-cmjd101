9f6c3087d7f466eedff547099fe5f3e7e8f3b3c1
danu <danujagreru@gmail.com>
2023-07-02 10:48:43 +0530

    initial commit

---
+++ native-hibernate/nh-bootstrapping/src/main/java/lk/ijse/nh/AppInitializer.java
@@ -0,0 +1,12 @@
+package lk.ijse.nh;
+
+/*
+    @author DanujaV
+    @created 7/2/23 - 10:37 AM   
+*/
+
+public class AppInitializer {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ native-hibernate/nh-bootstrapping/src/main/java/lk/ijse/nh/util/HibernateUtil.java
@@ -0,0 +1,10 @@
+package lk.ijse.nh.util;
+
+/*
+    @author DanujaV
+    @created 7/2/23 - 10:38 AM   
+*/
+
+public class HibernateUtil {    //SessionFactory
+
+}

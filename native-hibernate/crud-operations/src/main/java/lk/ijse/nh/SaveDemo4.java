4fc2ee39fdb459e949e6a09bde66c649a00acaf0
danu <danujagreru@gmail.com>
2023-07-02 15:11:55 +0530

    hibernate Object State

--- native-hibernate/crud-operations/src/main/java/lk/ijse/nh/SaveDemo4.java
+++ native-hibernate/crud-operations/src/main/java/lk/ijse/nh/SaveDemo4.java
@@ -11,7 +11,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
-public class SaveDemo3 {
+public class SaveDemo4 {
     public static void main(String[] args) {
         try (SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
              Session session = sessionFactory.openSession()) {
@@ -27,9 +27,11 @@ public class SaveDemo3 {
             System.out.println("item persist");
             System.out.println("does i1 inside the context: " + session.contains(i1));  //?true
 
-            i1.setDescription("Biscuit");
-            System.out.println("change the description");
-            System.out.println("does i1 inside the context: " + session.contains(i1));  //true
+            session.detach(i1);
+//            session.evict(i1);
+            i1.setDescription("Ice-cream");
+            System.out.println("change the description to Ice-cream");
+            System.out.println("does i1 inside the context: " + session.contains(i1));  //?
 
             transaction.commit();
 

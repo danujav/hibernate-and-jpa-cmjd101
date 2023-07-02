686b8442a694e287f53ee4a32bfa6a791c835723
danu <danujagreru@gmail.com>
2023-07-02 11:44:40 +0530

    check session availability

--- native-hibernate/nh-bootstrapping/src/main/java/lk/ijse/nh/AppInitializer.java
+++ native-hibernate/nh-bootstrapping/src/main/java/lk/ijse/nh/AppInitializer.java
@@ -5,8 +5,20 @@ package lk.ijse.nh;
     @created 7/2/23 - 10:37 AM   
 */
 
+import lk.ijse.nh.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 public class AppInitializer {
     public static void main(String[] args) {
+        //try with resource
+        try(SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+            Session session = sessionFactory.openSession()) {
 
+            System.out.println("session: " + session);
+        }
+
+//        session.close();
+//        sessionFactory.close();
     }
 }

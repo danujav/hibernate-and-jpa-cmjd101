9dee695a78bcb423612dd862d99230be01e800c9
danu <danujagreru@gmail.com>
2023-07-02 13:49:09 +0530

    bootstrapping crud-operation project

---
+++ native-hibernate/crud-operations/src/main/java/lk/ijse/nh/util/HibernateUtil.java
@@ -0,0 +1,38 @@
+package lk.ijse.nh.util;
+
+/*
+    @author DanujaV
+    @created 7/2/23 - 1:40 PM   
+*/
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+//        3.1.4
+        StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+//                .configure("/hibernate.cfg.xml")
+                .loadProperties("/application.properties")
+                .build();
+
+        Metadata metadata = new MetadataSources(standardRegistry)
+                .getMetadataBuilder()
+                .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+                .build();
+
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder()
+                .build();
+
+        return sessionFactory;
+    }
+    public static SessionFactory getSessionFactory () {
+        return sessionFactory;
+    }
+}

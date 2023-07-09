29c5d143c2e37dfe2ec89620bdc0c7a4778feab5
danu <danujagreru@gmail.com>
2023-07-09 09:04:58 +0530

    bootstrapping hibernate

---
+++ native-hibernate/relationships/src/main/java/lk/ijse/relationships/AppInitializer.java
@@ -0,0 +1,24 @@
+package lk.ijse.relationships;
+
+/*
+    @author DanujaV
+    @created 7/2/23 - 10:37 AM   
+*/
+
+import lk.ijse.relationships.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        //try with resource
+        try(SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+            Session session = sessionFactory.openSession()) {
+
+            System.out.println("session: " + session);
+        }
+
+//        session.close();
+//        sessionFactory.close();
+    }
+}
---
+++ native-hibernate/relationships/src/main/java/lk/ijse/relationships/util/HibernateUtil.java
@@ -0,0 +1,39 @@
+package lk.ijse.relationships.util;
+
+/*
+    @author DanujaV
+    @created 7/2/23 - 10:38 AM   
+*/
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {    //SessionFactory
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+//        3.1.4
+        StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+//                .configure("/hibernate.cfg.xml")
+                .loadProperties("/application.properties")
+                .build();
+
+        Metadata metadata = new MetadataSources(standardRegistry)
+                .getMetadataBuilder()
+                .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+                .build();
+
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder()
+                .build();
+
+        return sessionFactory;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
